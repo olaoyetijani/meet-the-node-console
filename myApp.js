92ea84c7676d2428f4f3b238ce5d9422eab5a698
olaoyetijani <adeolatijani12@gmail.com>
2021-12-28 21:54:07 +0100

    route problem

--- myApp.js
+++ myApp.js
@@ -9,13 +9,14 @@ app.use( (req, res, next) => {
 });
 
 
-app.get("/", (req, res) => {
+ app.get("/", (req, res) => {
     res.send("Hello Express");
-});
+}); 
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + /views/index.html);
-});
+    res.sendFile(__dirname + "/views/index.html");
+}); 
+ 
 
 app.use("/public", express.static(__dirname + "/public"));
 
