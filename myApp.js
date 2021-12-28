b059039971909cb69769f10b9309ff7ea13319f2
olaoyetijani <adeolatijani12@gmail.com>
2021-12-28 21:01:20 +0100

    first commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var app = express();
+console.log("Hello World");
+
+
+app.use( (req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + /views/index.html);
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+    res.json({"message" : "Hello json"});
+});
+
 
 
 
